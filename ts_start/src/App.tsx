50d9205b4426b27ba1ae463887c87076998d4979
jinsimpunch <jinsimpunch1006@gmail.com>
2023-03-09 12:43:00 +0900

    ts.ver theme

--- ts_start/src/App.tsx
+++ ts_start/src/App.tsx
@@ -1,11 +1,21 @@
+import styled from "styled-components";
 import Circle from "./Circle";
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`
+const H1 = styled.h1`
+  color:${(porps) => porps.theme.textColor}
+`
+
 
 function App() {
   return (
-    <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>
+        hi
+      </H1>
+    </Container>
   );
 }
 
